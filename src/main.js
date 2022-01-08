200c28cb5a4116a1b1c393713bbb4be59cf6167c
张吉祥 <1422810115@qq.com>
2022-01-08 14:11:14 +0800

    引入vant组件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
 Vue.config.productionTip = false
+
+// 引入vant全部组件
+import Vant from "vant"
+import "vant/lib/index.css"
+Vue.use(Vant)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
